fc6cfe96f94a050f5c7315c75043d16924741ccb
nafiz <Nafiz228.haru@gmail.com>
2026-04-14 23:31:58 +0900

    Aiドクター蘇生時間修正

--- resources/[general]/hh_aidoc/config.lua
+++ resources/[general]/hh_aidoc/config.lua
@@ -4,6 +4,6 @@
 Config = {}
 
 Config.Doctor = 0 -- Minimum Amount of EMS to work
-Config.Price = 2000
-Config.ReviveTime = 20000  --in msec
+Config.Price = 2000 --money
+Config.ReviveTime = 10000  --in msec
 
