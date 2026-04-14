c5ff24497031e21805298cb7eb40f7abd78e04be
nafiz <Nafiz228.haru@gmail.com>
2026-04-15 03:22:08 +0900

    銀行UI変更

--- resources/[qb]/qb-banking/client.lua
+++ resources/[qb]/qb-banking/client.lua
@@ -155,7 +155,7 @@ if Config.useTarget then
                 options = {
                     {
                         icon = 'fas fa-university',
-                        label = 'Open Bank',
+                        label = '銀行を開く',
                         action = function()
                             OpenBank()
                         end,
@@ -173,7 +173,7 @@ if Config.useTarget then
                 options = {
                     {
                         icon = 'fas fa-university',
-                        label = 'Open ATM',
+                        label = 'ATMを開く',
                         item = 'bank_card',
                         action = function()
                             OpenATM()
@@ -204,7 +204,7 @@ if not Config.useTarget then
         combo:onPlayerInOut(function(isPointInside)
             isPlayerInsideBankZone = isPointInside
             if isPlayerInsideBankZone then
-                exports['qb-core']:DrawText('Open Bank')
+                exports['qb-core']:DrawText('[E] 銀行を開く')
                 CreateThread(function()
                     while isPlayerInsideBankZone do
                         Wait(0)
